21b7624c73add124c6fab2788c43e719dbb612f2
sunny kushwaha <sunnysadhana45@gmail.com>
2018-10-13 17:05:02 +0530

    first commit

---
+++ app/auth/auth0-variables.ts
@@ -0,0 +1,12 @@
+interface AuthConfig {
+    clientID: string;
+    domain: string;
+    callbackURL: string;
+  }
+  
+  export const AUTH_CONFIG: AuthConfig = {
+    clientID: 'RbVw9cSNLIKsjcH3vvRsl1BwTqGlg55i',
+    domain: 'singhakshit.auth0.com',
+    callbackURL: 'http://localhost:4200/callback'
+  };
+  
